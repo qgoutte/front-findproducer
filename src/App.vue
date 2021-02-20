25d398af3765a56e432b09a57d70b4ae0d3756f6
qgoutte <quentingoutte@gmail.com>
2021-02-20 15:44:39 +0100

    correct the dependance of primevue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <InputText/>
+    <Button label="Greet"></Button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
-import App from './App.vue'
+import App from './App.vue';
 
-const app = createApp(App).mount('#app');
+import 'primevue/resources/themes/saga-blue/theme.css';     //theme
+import 'primevue/resources/primevue.min.css';      //core css
+import 'primeicons/primeicons.css';                 //icons
+
+
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+
+
+const app = createApp(App);
 app.use(PrimeVue);
+app.component('InputText', InputText);
+app.component('Button', Button);
+
+app.mount("#app");
